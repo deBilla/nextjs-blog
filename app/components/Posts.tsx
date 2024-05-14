f0b32d2d70372080605e79e6063600d44c2a1fc8
debilla <dimuthu.billa@gmail.com>
2024-05-14 23:05:04 +0800

    Removing movie part from blog

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -1,7 +1,39 @@
 import React from "react";
 import Card from "./Card";
 import parse from "rss-to-json";
-import { RSSObject, RSSResponse } from "./Movies";
+
+export type RSSThumbnail = {
+  medium: string;
+  url: string;
+}
+
+export type RSSMedia = {
+  thumbnail: RSSThumbnail;
+}
+
+export type RSSObject = {
+  id: string;
+  title: string;
+  description: string;
+  link: string;
+  author: string;
+  published: number;
+  created: number;
+  category: string[];
+  content: string;
+  enclosure: any;
+  media: RSSMedia;
+};
+
+export type RSSResponse = {
+  title: string;
+  description: any;
+  link: string;
+  image: string;
+  category: any[];
+  items: RSSObject[];
+};
+
 
 const Posts = async () => {
   const mediumResponse: RSSResponse = await parse('https://medium.com/feed/@billa-code');
